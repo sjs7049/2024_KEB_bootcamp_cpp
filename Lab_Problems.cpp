ede910c9691113ea82a46cb85655f3a7762b384f
sjs7049 <sjs7049@gmail.com>
2024-01-25 17:17:35 +0900

    c++ program 5-22

--- Lab_Problems.cpp
+++ Lab_Problems.cpp
@@ -1,34 +1,38 @@
-// 최솟값과 최댓값 찾기
+// 적어도 또는 모두 조건에 충족하는 숫자 찾기
+/*
+결과
+리스트의 크기를 입력하세요: 5
+다음 숫자를 입력하세요: 12
+다음 숫자를 입력하세요: 32
+다음 숫자를 입력하세요: 28
+28은(는) 7로 나눌 수 있습니다.
+*/
 
 #include <iostream>
-#include <limits>
 using namespace std;
 
 int main()
 {
     int size, num;
-    int max = numeric_limits<int>::min();
-    int min = numeric_limits<int>::max();
+    bool found = false;
 
     do
     {
-        cout << "음수가 아닌 수를 리스트의 크기를 입력하세요: ";
+        cout << "리스트의 크기를 입력하세요: ";
         cin >> size;
-    } while (size < 0);
-    
-    for(int i = 0; i < size; i++){
+    } while(size < 0);
+
+    for(int i = 0; (i < size) && (!found); i++){
         cout << "다음 숫자를 입력하세요: ";
         cin >> num;
-
-        if(num > max){
-            max = num;
-        }
-        if(num < min){
-            min = num;
-        }
+        found = (num % 7 == 0);
     }
 
-    cout << "최솟값 = " << min << endl;
-    cout << "최댓값 = " << max << endl;
+    if(found){
+        cout << num << "은(는) 7로 나눌 수 있습니다." << endl;
+    }
+    else{
+        cout << "리스트 내부에 7로 나눌 수 있는 숫자가 없습니다." << endl;
+    }
     return 0;
 }
