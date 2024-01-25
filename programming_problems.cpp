1211da329997200e68b275a816654c8767da2579
sjs7049 <sjs7049@gmail.com>
2024-01-25 18:28:38 +0900

    c++ practice 3-9

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,29 +1,32 @@
-// 3-8. 시간을 시, 분, 초 단위로 입력받았을 때, 이를 초 단위로 변환하는 프로그램을 만드세요.
+// 3-9. 초 단위(long 자료형)을 입력받았을 떄, 이를 시, 분, 초 단위로 변환하는 프로그램을 만드세요.
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int hours, minutes, seconds;
-    int convertSeconds;
+    long secondsInput;
+    int hours{0}, minutes{0}, seconds{0};
 
-    cout << "시간을 시, 분, 초 단위로 입력하세요: ";
-    cin >> hours >> minutes >> seconds;
+    cout << "초 단위를 입력하세요: ";
+    cin >> secondsInput;
 
-    if(hours > 0){
-        convertSeconds += (hours * 3600);
-        if(minutes > 0){
-            convertSeconds += (minutes * 60);
+    if(secondsInput >= 3600){
+        hours = secondsInput / 3600;
+        if((secondsInput % 3600) >= 60){
+            minutes = (secondsInput % 3600) / 60;
+            seconds = ((secondsInput % 3600) % 60);
         }
     }
     else{
-        if(minutes > 0){
-            convertSeconds += (minutes * 60);
+        if((secondsInput % 3600) >= 60){
+            minutes = (secondsInput % 3600) / 60;
+            seconds = ((secondsInput % 3600) % 60);
+        }
+        else{
+            seconds = secondsInput;
         }
     }
-
-    convertSeconds += seconds;
-    cout << "초 단위로 변환한 값은 " << convertSeconds << endl;
-    return 0;
     
+    cout << "시, 분, 초 단위로 변환한 값: " << hours << "시 " << minutes << "분 " << seconds << "초" << endl;
+    return 0;
 }
