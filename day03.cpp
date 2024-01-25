564070546ef50d4af1666f2f96fa363321197343
sjs7049 <sjs7049@gmail.com>
2024-01-25 14:37:50 +0900

    c++ program 6-12

--- day03.cpp
+++ day03.cpp
@@ -1,23 +1,40 @@
-// 두 수를 입력 받고 큰 숫자 출력하는 프로그램
+// 윤년인지 확인하기
 
 #include <iostream>
 using namespace std;
 
-int larger(int num1, int num2){
-    int largeNumber = (num1 > num2 ? num1 : num2);
-     
-    return largeNumber;
+int input()
+{
+    int y;
+    do
+    {
+        cout << "Enter a year after 1582: ";
+        cin >> y;
+    } while (y < 1582);
+    
+    return y;
+}
+
+bool process(int y){
+    bool cretical1 = (y % 4 == 0);
+    bool cretical2 = (y % 100 != 0)||(y % 400 == 0);
+
+    return (cretical1) && (cretical2);
+}
+
+void output(int y, bool result){
+    if(result){
+        cout << "Year " << y << " is a leap year." << endl;
+    }
+    else{
+        cout << "Year " << y << " is not a leap year." << endl;
+    }
 }
 
 int main()
 {
-    int n1, n2;
-
-    cout << "Input the first number: ";
-    cin >> n1;
-    cout << "Input the second number: ";
-    cin >> n2;
-
-    cout << "The large number is " << larger(n1, n2) << endl;
+    int year = input();
+    bool result = process(year);
+    output(year, result);
     return 0;
 }
