328f1cfc03366ec669a03ba884085ea73fbb6f5a
sjs7049 <sjs7049@gmail.com>
2024-01-25 15:21:58 +0900

    c++ program 6-22

--- day03.cpp
+++ day03.cpp
@@ -1,25 +1,12 @@
-// 함수 오버로딩
-
 #include <iostream>
 using namespace std;
 
-int max(int num1, int num2){
-    int larger = (num1 > num2 ? num1 : num2);
-    return larger;
-}
-
-int max(int num1, int num2, int num3){
-    return max(max(num1, num2), num3);
-}
-
-int max(int num1, int num2, int num3, int num4){
-    return max(max(max(num1, num2), num3), num4);   
-}
+int num = 5;
 
 int main()
 {
-    cout << "maximum(5, 7) = " << max(5,7) << endl;
-    cout << "maximum(7, 9, 8) = " << max(7,9,8) << endl;
-    cout << "maximum(14, 3, 12, 11) = " << max(14,3,12,11) << endl;
+    int num = 25;
+    cout << "전역 변수 num의 값 = " << ::num << endl;
+    cout << "지역 변수 num의 값 = " << num << endl;
     return 0;
 }
