095d3f59a52541b1cfd88ce1b0e89d15fa6706bb
sjs7049 <sjs7049@gmail.com>
2024-01-25 15:13:51 +0900

    c++ program 6-19

--- day03.cpp
+++ day03.cpp
@@ -1,21 +1,25 @@
-// 변수 swqp하기
+// 함수 오버로딩
 
 #include <iostream>
 using namespace std;
 
-void swap(int& x, int& y){
-    int tmp = x;
-    x = y;
-    y = tmp;
+int max(int num1, int num2){
+    int larger = (num1 > num2 ? num1 : num2);
+    return larger;
+}
+
+int max(int num1, int num2, int num3){
+    return max(max(num1, num2), num3);
+}
+
+int max(int num1, int num2, int num3, int num4){
+    return max(max(max(num1, num2), num3), num4);   
 }
 
 int main()
 {
-    int x = 10;
-    int y = 20;
-    swap(x, y);
-
-    cout << "swqp 후 x 값 = " << x << endl;
-    cout << "swap 후 y 값 = " << y << endl;
+    cout << "maximum(5, 7) = " << max(5,7) << endl;
+    cout << "maximum(7, 9, 8) = " << max(7,9,8) << endl;
+    cout << "maximum(14, 3, 12, 11) = " << max(14,3,12,11) << endl;
     return 0;
 }
