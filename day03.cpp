850a6dfb0cec1ae4c4d2cbff6c471b7600e0ed8e
sjs7049 <sjs7049@gmail.com>
2024-01-25 14:10:47 +0900

    c++ program 6-10

--- day03.cpp
+++ day03.cpp
@@ -1,22 +1,23 @@
+// 두 수를 입력 받고 큰 숫자 출력하는 프로그램
+
 #include <iostream>
 using namespace std;
 
-void pattern(int patternsize){
-    for(int i = 1; i <= patternsize; i++){
-        for(int j = 1; j <= patternsize; j++){
-            cout << "*";
-        }
-        cout << endl;
-    }
+int larger(int num1, int num2){
+    int largeNumber = (num1 > num2 ? num1 : num2);
+     
+    return largeNumber;
 }
 
 int main()
 {
-    int patternSize;
-    
-    cout << "패턴의 크기를 입력하세요: ";
-    cin >> patternSize;
+    int n1, n2;
 
-    pattern(patternSize);
+    cout << "Input the first number: ";
+    cin >> n1;
+    cout << "Input the second number: ";
+    cin >> n2;
+
+    cout << "The large number is " << larger(n1, n2) << endl;
     return 0;
 }
