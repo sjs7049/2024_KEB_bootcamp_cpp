4aecf5faf5a6690a93427219360d78a58c05ef26
sjs7049 <sjs7049@gmail.com>
2024-01-26 16:13:07 +0900

    c++ practice 4-3

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,25 +1,57 @@
-// 4-2. 3개의 정수를 입력받고, 가장 작은 것을 출력하는 프로그램을 만드세요.
+// 4-3. 사용자로부터 1~12 사이의 숫자를 입력받고 해당 월을 January, ... December와 
+// 같은 형태의 영어로 출력하는 프로그램을 만드세요.
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int num1, num2, num3;
-    int min;
+    int monthNumber;
 
-    cout << "3개의 정수를 입력하세요: ";
-    cin >> num1 >> num2 >> num3;
-
-    if(num1 < num2 && num1 < num3){
-        min = num1;
+    do
+    {
+        cout << "Input the number between 1 and 12: ";
+        cin >> monthNumber;
+    } while (monthNumber < 1 && monthNumber > 12);
+    
+    switch (monthNumber)
+    {
+    case 1:
+        cout << "January" << endl;
+        break;
+    case 2:
+        cout << "February" << endl;
+        break;
+    case 3:
+        cout << "March" << endl;
+        break;
+    case 4:
+        cout << "April" << endl;
+        break;
+    case 5:
+        cout << "May" << endl;
+        break;
+    case 6:
+        cout << "June" << endl;
+        break;
+    case 7:
+        cout << "July" << endl;
+        break;
+    case 8:
+        cout << "August" << endl;
+        break;
+    case 9:
+        cout << "September" << endl;
+        break;
+    case 10:
+        cout << "October" << endl;
+        break;
+    case 11:
+        cout << "November" << endl;
+        break;
+    case 12:
+        cout << "December" << endl;
+        break;
     }
-    else if(num2 < num1 && num2 < num3){
-        min = num2;
-    }
-    else{
-        min = num3;
-    }
-    cout << "가장 작은 수는 " << min << endl;
     return 0;
 }
