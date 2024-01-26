0636538d34a4ed3fefa86ca6a91bd2f22acb9ead
sjs7049 <sjs7049@gmail.com>
2024-01-26 15:32:47 +0900

    c++ practice 4-1

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,22 +1,25 @@
-// 3-14. 1,400달러의 텔레비전, 220달러의 DVD 플레이어, 35.20달러의 리모컨 3가지 제품을
-// 판매하는 회사가 있습니다. 고객이 구입한 장비의 수를 입력으로 받는다고 할 때, 
-// 8.25%의 소비세가 추가된 전체 비용을 계산하는 프로그램을 만드세요.
+// 4-1. 사용자로부터 부호 없는 두 자리 정수를 입력받고, 
+// 자릿수의 값을 바꿔서 출력하는 프로그램을 만드세요. 79 9+
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    double television{1400}, dvd{220}, remoteControl{35.20};
-    int tvCnt, dvdCnt, rcCnt;
-    double totalPrice{0.00};
+    unsigned int num;
 
-    cout << "텔레비전, DVD 플레이어, 리모컨을 구입한 장비의 수를 각각 입력하세요: ";
-    cin >> tvCnt >> dvdCnt >> rcCnt;
-
-    totalPrice = television * tvCnt + dvd * dvdCnt + remoteControl * rcCnt;
-    totalPrice += (totalPrice * 0.0825);
-
-    cout << "전체 비용: " << totalPrice << "달러" << endl;
+    do
+    {
+        cout << "부호 없는 두 자리 정수 입력: ";
+        cin >> num;
+    } while (num < 10 && num > 99);
+    
+    int tmp = 0;
+    while(num != 0){
+        tmp *= 10;
+        tmp += num % 10;
+        num /= 10;
+    }
+    cout << "자릿수 바꾼 값: " << tmp << endl;
     return 0;
 }
