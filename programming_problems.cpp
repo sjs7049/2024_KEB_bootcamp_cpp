3cf530fc37c85930d115fd6ff56c883f6ea06d16
sjs7049 <sjs7049@gmail.com>
2024-01-26 14:22:55 +0900

    c++ practice 3-10

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,32 +1,23 @@
-// 3-9. 초 단위(long 자료형)을 입력받았을 떄, 이를 시, 분, 초 단위로 변환하는 프로그램을 만드세요.
+// 3-10. 4개의 점수(int)를 입력 받고, 이를 기반으로 평균 점수(double)를 찾아 출력하는 프로그램을 만드세요.
+
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    long secondsInput;
-    int hours{0}, minutes{0}, seconds{0};
+    int score;
+    int totalScore = 0;
+    double average;
 
-    cout << "초 단위를 입력하세요: ";
-    cin >> secondsInput;
+    cout << "4개의 점수를 입력하세요: " << endl;
+    for(int i = 0; i < 4; i++){
+        cin >> score;
+        totalScore += score;
+    }
 
-    if(secondsInput >= 3600){
-        hours = secondsInput / 3600;
-        if((secondsInput % 3600) >= 60){
-            minutes = (secondsInput % 3600) / 60;
-            seconds = ((secondsInput % 3600) % 60);
-        }
-    }
-    else{
-        if((secondsInput % 3600) >= 60){
-            minutes = (secondsInput % 3600) / 60;
-            seconds = ((secondsInput % 3600) % 60);
-        }
-        else{
-            seconds = secondsInput;
-        }
-    }
-    
-    cout << "시, 분, 초 단위로 변환한 값: " << hours << "시 " << minutes << "분 " << seconds << "초" << endl;
+    average = static_cast<double> (totalScore) / 4;
+    cout << "평균 점수는 " << fixed << setprecision(2) << average << endl;
+
     return 0;
 }
