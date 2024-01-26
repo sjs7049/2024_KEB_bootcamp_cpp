190d6d915a4d5c3fb68ba4a05afc1209820b88b2
sjs7049 <sjs7049@gmail.com>
2024-01-26 16:25:57 +0900

    c++ practice 4-4

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,57 +1,32 @@
-// 4-3. 사용자로부터 1~12 사이의 숫자를 입력받고 해당 월을 January, ... December와 
-// 같은 형태의 영어로 출력하는 프로그램을 만드세요.
+// 4-4 차량의 종류(일반 승용차는 'c', 버스는 'b', 트럭은 't')와 주차장에서 차량이 있던
+// 시간을 입력한 뒤, 다음과 같은 주차 요금에 따라서 요금을 계산하는 프로그램을 작성하세요.
+
+// 일반 승용차: 시간당 2달러, 버스: 시간당 3달러, 트럭: 시간당 4달러
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    int monthNumber;
+    char vehicleType;
+    int time, fee;
 
-    do
+    cout << "차량의 종류와 있던 시간 입력: ";
+    cin >> vehicleType >> time;
+
+    switch (vehicleType)
     {
-        cout << "Input the number between 1 and 12: ";
-        cin >> monthNumber;
-    } while (monthNumber < 1 && monthNumber > 12);
-    
-    switch (monthNumber)
-    {
-    case 1:
-        cout << "January" << endl;
+    case 'c':
+        fee = time * 2;
         break;
-    case 2:
-        cout << "February" << endl;
+    case 'b':
+        fee = time * 3;
         break;
-    case 3:
-        cout << "March" << endl;
-        break;
-    case 4:
-        cout << "April" << endl;
-        break;
-    case 5:
-        cout << "May" << endl;
-        break;
-    case 6:
-        cout << "June" << endl;
-        break;
-    case 7:
-        cout << "July" << endl;
-        break;
-    case 8:
-        cout << "August" << endl;
-        break;
-    case 9:
-        cout << "September" << endl;
-        break;
-    case 10:
-        cout << "October" << endl;
-        break;
-    case 11:
-        cout << "November" << endl;
-        break;
-    case 12:
-        cout << "December" << endl;
+    case 't':
+        fee = time * 4;
         break;
     }
+    cout << "주차 요금은 " << fee << "달러" << endl;
     return 0;
 }
