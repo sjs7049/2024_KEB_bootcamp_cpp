53da5a4fc8cb5b355fa8ffa638e8c9710b1e8c6c
sjs7049 <sjs7049@gmail.com>
2024-01-26 16:01:00 +0900

    c++ practice 4-2

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,25 +1,25 @@
-// 4-1. 사용자로부터 부호 없는 두 자리 정수를 입력받고, 
-// 자릿수의 값을 바꿔서 출력하는 프로그램을 만드세요. 79 9+
+// 4-2. 3개의 정수를 입력받고, 가장 작은 것을 출력하는 프로그램을 만드세요.
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    unsigned int num;
+    int num1, num2, num3;
+    int min;
 
-    do
-    {
-        cout << "부호 없는 두 자리 정수 입력: ";
-        cin >> num;
-    } while (num < 10 && num > 99);
-    
-    int tmp = 0;
-    while(num != 0){
-        tmp *= 10;
-        tmp += num % 10;
-        num /= 10;
+    cout << "3개의 정수를 입력하세요: ";
+    cin >> num1 >> num2 >> num3;
+
+    if(num1 < num2 && num1 < num3){
+        min = num1;
     }
-    cout << "자릿수 바꾼 값: " << tmp << endl;
+    else if(num2 < num1 && num2 < num3){
+        min = num2;
+    }
+    else{
+        min = num3;
+    }
+    cout << "가장 작은 수는 " << min << endl;
     return 0;
 }
