21e2984ad13b48d96b4ff2cccf1b47fde7d0453e
sjs7049 <sjs7049@gmail.com>
2024-01-26 14:36:14 +0900

    c++ practice 3-11

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,4 +1,5 @@
-// 3-10. 4개의 점수(int)를 입력 받고, 이를 기반으로 평균 점수(double)를 찾아 출력하는 프로그램을 만드세요.
+// 3-11. 주어진 섭씨 온도를 화씨 온도로 변환해서 출력하는 프로그램을 만드세요.
+// F = (9/5)*C + 32
 
 #include <iostream>
 #include <iomanip>
@@ -6,18 +7,13 @@ using namespace std;
 
 int main()
 {
-    int score;
-    int totalScore = 0;
-    double average;
+    double celsius;
+    cout << "섭씨 온도를 입력하세요: ";
+    cin >> celsius;
 
-    cout << "4개의 점수를 입력하세요: " << endl;
-    for(int i = 0; i < 4; i++){
-        cin >> score;
-        totalScore += score;
-    }
+    double fahrenheit = (9.0 / 5.0) * celsius + 32.0;
 
-    average = static_cast<double> (totalScore) / 4;
-    cout << "평균 점수는 " << fixed << setprecision(2) << average << endl;
-
+    cout << fixed << setprecision(2);
+    cout << "섭씨 온도를 화씨 온도로 변환한 값은 " << fahrenheit << endl;
     return 0;
 }
