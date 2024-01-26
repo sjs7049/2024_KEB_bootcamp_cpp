d4f2d91f9aca8a2d61d443a642b54e1d70a1e9f1
sjs7049 <sjs7049@gmail.com>
2024-01-26 14:38:25 +0900

    c++ practice 3-12

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,5 +1,5 @@
-// 3-11. 주어진 섭씨 온도를 화씨 온도로 변환해서 출력하는 프로그램을 만드세요.
-// F = (9/5)*C + 32
+// 3-12. 주어진 화씨 온도를 섭씨 온도로 변환해서 출력하는 프로그램을 만드세요.
+// C = (F-32)*(5/9)
 
 #include <iostream>
 #include <iomanip>
@@ -7,13 +7,13 @@ using namespace std;
 
 int main()
 {
-    double celsius;
-    cout << "섭씨 온도를 입력하세요: ";
-    cin >> celsius;
+    double fahrenheit;
+    cout << "화씨 온도를 입력하세요: ";
+    cin >> fahrenheit;
 
-    double fahrenheit = (9.0 / 5.0) * celsius + 32.0;
+    double celsius = (fahrenheit - 32.0) * (5.0 / 9.0);
 
     cout << fixed << setprecision(2);
-    cout << "섭씨 온도를 화씨 온도로 변환한 값은 " << fahrenheit << endl;
+    cout << "화씨 온도를 섭씨 온도로 변환한 값은 " << celsius << endl;
     return 0;
 }
