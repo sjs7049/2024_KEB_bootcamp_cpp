f94ab01c37ae54fc7828c8493ecb73b0e3b0507c
sjs7049 <sjs7049@gmail.com>
2024-01-26 15:20:39 +0900

    c++ practice 3-14

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,26 +1,22 @@
-// 3-13. 아르바이트로 일주일에 기본 40시간을 일한다고 가정
-// 40시간 넘게 일을 하는 경우, 넘는 시급은 60%가 추가로 지급됨
-// 일주일의 기본 급여와 추가로 일한 시간을 입력했을 때, 전체 급여를 구하는 프로그램 작성
+// 3-14. 1,400달러의 텔레비전, 220달러의 DVD 플레이어, 35.20달러의 리모컨 3가지 제품을
+// 판매하는 회사가 있습니다. 고객이 구입한 장비의 수를 입력으로 받는다고 할 때, 
+// 8.25%의 소비세가 추가된 전체 비용을 계산하는 프로그램을 만드세요.
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    long baseSalary, totalSalary;
-    long addSalary = 0.0;
-    int hoursWorked = 40;
-    int overHours;
+    double television{1400}, dvd{220}, remoteControl{35.20};
+    int tvCnt, dvdCnt, rcCnt;
+    double totalPrice{0.00};
 
-    cout << "기본 급여 입력: ";
-    cin >> baseSalary;
-    cout << "추가로 일한 시간 입력: ";
-    cin >> overHours;
+    cout << "텔레비전, DVD 플레이어, 리모컨을 구입한 장비의 수를 각각 입력하세요: ";
+    cin >> tvCnt >> dvdCnt >> rcCnt;
 
-    if(hoursWorked + overHours > 40){
-        addSalary = (baseSalary * overHours * 0.6);
-    }
-    totalSalary = (hoursWorked + overHours) * baseSalary + addSalary;
-    cout << "전체 급여 : " << totalSalary << endl;
+    totalPrice = television * tvCnt + dvd * dvdCnt + remoteControl * rcCnt;
+    totalPrice += (totalPrice * 0.0825);
+
+    cout << "전체 비용: " << totalPrice << "달러" << endl;
     return 0;
-} 
+}
