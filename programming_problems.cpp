9f71657dc18eb6222cb5b438e3d14fe5767adfbc
sjs7049 <sjs7049@gmail.com>
2024-01-27 21:36:34 +0900

    c++ practice 5-12

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,32 +1,15 @@
-// 5-11. 0~100의 범위에 있는 점수 리스트를 읽어 들이고, 가장 작은 점수와 가장 큰 점수를 출력하세요.
-// 사용자로부터 점수를 읽어 들일 때는 적어도 1개, 그리고 10개 이하로만 읽어 들이게 하세요.
+// 5-12. 2000년부터 2009년까지의 범위에 있는 윤년을 출력하는 프로그램을 만드세요.
+// 1년을 400으로 나눌 수 있다면 윤년입니다. 또는 4로 나눌 수 있지만 100으로 나눌 수 없으면 윤년입니다.
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int score;
-    int max{0}, min{100};
-    int cnt = 0;
-
-    cout << "Input the number between 0 and 100: ";
-    while(cin >> score){
-        cnt++;
-        if(cnt > 0 && cnt <=10){
-            if(score > max){
-            max = score;
-            }
-            if(score < min){
-                min = score;
-            }
-            cout << "Input the number between 0 and 100: ";
-        }
-        else{
-            break;
+    for(int i = 2000; i <= 2009; i++){
+        if((i % 400 == 0) || (i % 4 == 0 && i % 100 != 0)){
+            cout << i << " ";
         }
     }
-    cout << "\nThe max of scores is " << max << endl;
-    cout << "The min of scores is " << min << endl;
     return 0;
 }
