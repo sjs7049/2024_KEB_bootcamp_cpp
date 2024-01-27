fd6a76a855a1ef5d16098f81593321f67ed636b3
sjs7049 <sjs7049@gmail.com>
2024-01-27 22:12:13 +0900

    c++ practice 5-16

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,34 +1,22 @@
-// 5-15. 사용자로부터 양의 정수를 입력받고 회문 숫자인지 확인하는 프로그램을 만드세요.
-// 회문 숫자는 자릿수를 반대로 해도 원래 숫자와 같아지는 숫자를 의미합니다.
-
+// 5-16. 0~99의 범위에 있는 모든 정수 도씨 온도(Celsius)를 화씨 온도(Fahrenheit)로 
+// 변환해서 표 형태로 출력하는 프로그램을 만드세요.
+// 화씨 = 도씨 * 1.8 + 32
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    int n, tmp;
-    int res = 0;
+    int celsius;
 
     do
     {
-        cout << "Input the positive integer: ";
-        cin >> n;
-    } while (n <= 0);
+        cout << "Input the Celsius between 0 and 99: ";
+        cin >> celsius;
+    } while(celsius < 0 || celsius > 99);
 
-    int m = n;
-    while (m > 0)
-    {
-        tmp = m % 10;
-        res = res*10 + tmp;
-        m /= 10;
-    }
-    
-    if(res == n){
-        cout << n << " is a palindrome." << endl;
-    }
-    else{
-        cout << n << " is NOT a palindrome." << endl;
-    }
+    double fahrenheit = celsius * 1.8 + 32.0;
+    cout << "The Fahrenheit of " << celsius << "C is " << fahrenheit << "F" << endl;
     return 0;
 }
