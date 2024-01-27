dc20e5cee05af1e5f0f36d403574b8f222f90021
sjs7049 <sjs7049@gmail.com>
2024-01-27 17:46:21 +0900

    c++ practice 5-6

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,41 +1,13 @@
-// 5-5. 사용자에게 두 양의 정수를 입력받고 두 정수를 범위로 내부에 있는 
-// 홀수와 짝수를 출력하는 프로그램을 만드세요.
+// 5-6. 1~100의 범위에 있는 정수 중에서 7로 나눌 수 있는 숫자들을 출력하세요.
 
 #include <iostream>
 using namespace std;
 
 int main()
 {   
-    int num1, num2;
-
-    do
-    {
-        cout << "Input the first positive integer: ";
-        cin >> num1;
-    } while (num1 <= 0);
-
-    do
-    {
-        cout << "Input the second positive integer: ";
-        cin >> num2;
-    } while (num2 <= 0);
-
-    if(num1 > num2){
-        int tmp = num1;
-        num1 = num2;
-        num2 = tmp;        
-    }  
-
-    cout << "\nOdd and even numbers between " << num1 << " and " << num2 << endl;
-    cout << "Even numbers: ";
-    for(int i = (num1 + 1); i < num2; i++){
-        if(i % 2 == 0){
-            cout << i << " ";
-        }
-    }
-    cout << "\nOdd numbers: ";
-    for(int i = (num1 + 1); i < num2; i++){
-        if(i % 2 != 0){
+    cout << "The number between 1 and 100 that is divisible by 7" << endl;
+    for(int i = 1; i < 100; i++){
+        if(i % 7 == 0){
             cout << i << " ";
         }
     }
