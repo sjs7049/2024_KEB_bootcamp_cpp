3a29a594d4b9c309813b184da0edb234e5a17fe1
sjs7049 <sjs7049@gmail.com>
2024-01-27 22:58:39 +0900

    c++ practice 6-8

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,4 +1,4 @@
-// 6-7. rand 함수를 사용해서 소수점 뒤에 한 자리만 있는 랜덤한 숫자를 10개 출력하는 프로그램을 작성하세요.
+// 6-8. 랜덤한 숫자를 2, 4, 6, 8, 10 중에서만 만들어 10개를 출력하는 프로그램을 작성하세요.
 
 #include <iostream>
 #include <cstdlib>
@@ -9,7 +9,7 @@ int main()
     srand(time(0));
 
     for(int i = 0; i < 10; i++){
-        double tmp = (rand() % (1000 - 1 + 1) + 1) / 10.0;
+        int tmp = (rand() % 5 + 1) * 2;
         cout << tmp << " ";
     }
     cout << endl;
