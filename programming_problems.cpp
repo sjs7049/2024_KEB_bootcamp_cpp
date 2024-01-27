e102ae0a5446fe91debb4714f55e0dc28070805d
sjs7049 <sjs7049@gmail.com>
2024-01-27 15:05:32 +0900

    c++ practice 4-9

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,31 +1,57 @@
-// 4-8. 사용자로부터 점의 좌표 x와 y를 입력받고, 직교 좌표계에서 점이 몇 사분면에
-// 위치하는지를 출력하는 프로그램을 만드세요. 
+// 4-9. [프로그램 4-17]에 윤년의 개념을 추가해서 프로그램 수정하세요.
+// 프로그램은 사용자로부터 추가 연도를 입력받으세요.
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int x, y;
+    int year;
+    int month;
+    int day;
+    int totalDays = 0;
 
-    cout << "점의 좌표 x와 y를 입력하세요: ";
-    cin >> x >> y;
+    cout << "몇 년인지 입력하세요: ";
+    cin >> year;
+    cout << "몇 월인지 입력하세요: ";
+    cin >> month;
+    cout << "며칠인지 입력하세요: "; 
+    cin >> day;
 
-    if(x > 0 && y > 0){
-        cout << "제 1사분면" << endl;
+    bool leapYear = (year % 400) || (year % 4 && ! (year % 100));
+
+    int m01 = 31;
+    int m02;
+    if(leapYear){ m02 = 29; }
+    else{ m02 = 28; }
+    int m03 = 31;
+    int m04 = 30;
+    int m05 = 31;
+    int m06 = 30;
+    int m07 = 31;
+    int m08 = 31;
+    int m09 = 30;
+    int m10 = 31;
+    int m11 = 30;
+
+    switch(month)
+    {
+    case 12 : totalDays += m11;
+    case 11 : totalDays += m10;
+    case 10 : totalDays += m09;
+    case 9  : totalDays += m08;
+    case 8  : totalDays += m07;
+    case 7  : totalDays += m06;
+    case 6  : totalDays += m05;
+    case 5  : totalDays += m04;
+    case 4  : totalDays += m03;
+    case 3  : totalDays += m02;
+    case 2  : totalDays += m01;
+    case 1  : totalDays += 0;
     }
-    else if(x < 0 && y > 0){
-        cout << "제 2사분면" << endl;
-    }
-    else if(x < 0 && y < 0){
-        cout << "제 3사분면" << endl;
-    }
-    else if(x > 0 && y < 0){
-        cout << "제 4사분면" << endl;
-    }
-    else{
-        cout << "어떠한 사분면도 아닙니다." << endl;
-    }
-    return 0;
-    
+
+    totalDays +=  day;
+    cout << leapYear << endl;
+    cout << "올해의 " << totalDays << "번째 날입니다.";
+    return 0;      
 }
