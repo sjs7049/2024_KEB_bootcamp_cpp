c39c11f77bf729a77e06cdaea1c98cf0a427cb9f
sjs7049 <sjs7049@gmail.com>
2024-01-27 17:49:08 +0900

    c++ practice 5-7

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,13 +1,13 @@
-// 5-6. 1~100의 범위에 있는 정수 중에서 7로 나눌 수 있는 숫자들을 출력하세요.
+// 5-7. 1~100의 범위에 있는 정수 중에서 5와 7로 동시에 나눌 수 있는 숫자들을 출력하세요.
 
 #include <iostream>
 using namespace std;
 
 int main()
 {   
-    cout << "The number between 1 and 100 that is divisible by 7" << endl;
+    cout << "The numbers between 1 and 100 that is divisible by 5 and 7" << endl;
     for(int i = 1; i < 100; i++){
-        if(i % 7 == 0){
+        if(i % 35 == 0){
             cout << i << " ";
         }
     }
