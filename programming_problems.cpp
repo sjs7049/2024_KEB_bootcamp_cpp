0704d206ca8da6e16b7be737f44d308fdc59e40d
sjs7049 <sjs7049@gmail.com>
2024-01-27 17:56:00 +0900

    c++ practice 5-8

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,13 +1,20 @@
-// 5-7. 1~100의 범위에 있는 정수 중에서 5와 7로 동시에 나눌 수 있는 숫자들을 출력하세요.
+// 5-8. 1~100의 범위에 있는 양의 정수를 읽어 들이고 약수를 출력하세요.
 
 #include <iostream>
 using namespace std;
 
 int main()
 {   
-    cout << "The numbers between 1 and 100 that is divisible by 5 and 7" << endl;
-    for(int i = 1; i < 100; i++){
-        if(i % 35 == 0){
+    int num;
+
+    do
+    {
+        cout << "Input the positive integer between 1 and 100: ";
+        cin >> num;
+    } while(num <= 0);
+
+    for(int i = 1; i <= num; i++){
+        if(num % i == 0){
             cout << i << " ";
         }
     }
