8b86262c52705f911f3f7555bdb795d0285725c3
sjs7049 <sjs7049@gmail.com>
2024-01-27 22:01:07 +0900

    c++ practice 5-15

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,5 +1,6 @@
-// 5-14. 사용자로부터 양의 정수를 입력받고 자릿수를 반대로 해서 출력하는 프로그램을 만드세요.
-// 예를 들어 359를 입력했다면 953 출력
+// 5-15. 사용자로부터 양의 정수를 입력받고 회문 숫자인지 확인하는 프로그램을 만드세요.
+// 회문 숫자는 자릿수를 반대로 해도 원래 숫자와 같아지는 숫자를 의미합니다.
+
 
 #include <iostream>
 using namespace std;
@@ -15,13 +16,19 @@ int main()
         cin >> n;
     } while (n <= 0);
 
-    while (n > 0)
+    int m = n;
+    while (m > 0)
     {
-        tmp = n % 10;
+        tmp = m % 10;
         res = res*10 + tmp;
-        n /= 10;
+        m /= 10;
     }
     
-    cout << "The integer with a digit reversed is " << res << endl;
+    if(res == n){
+        cout << n << " is a palindrome." << endl;
+    }
+    else{
+        cout << n << " is NOT a palindrome." << endl;
+    }
     return 0;
 }
