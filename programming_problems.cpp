39c879eec7f5790c8083760e2dd937ed539c1886
sjs7049 <sjs7049@gmail.com>
2024-01-27 23:05:32 +0900

    c++ practice 6-10

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,4 +1,5 @@
-// 6-9. 1~10 범위 정수의 제곱근을 출력하는 프로그램을 작성하세요.
+// 6-10. 1~10 범위의 정수로 사용해서 log2^x
+// log2^x = log10^x / log10^2
 
 #include <iostream>
 #include <cmath>
@@ -7,7 +8,7 @@ using namespace std;
 int main()
 {
     for(int i = 1; i <= 10; i++){
-        cout << sqrt(i) << " ";
+        cout << log10(i) / log10(2) << " ";
     }
     cout << endl;
     return 0;
