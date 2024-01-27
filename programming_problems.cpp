060c86271c0922331d2f9a495889891c086a28c0
sjs7049 <sjs7049@gmail.com>
2024-01-27 22:49:25 +0900

    c++ practice 6-7

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,6 +1,4 @@
-// 6-6. 동전을 던졌을 때 앞면 또는 뒷면이 나오는 것처럼, rand 함수를 사용해서 0 또는 1만
-// 나오게 하는 코드를 작성하고, 이를 활용해서 동전을 100만번 던지고
-// 앞면과 뒷면이 몇 번 나오는지 모방해서 확인하는 프로그램을 작성하세요. 
+// 6-7. rand 함수를 사용해서 소수점 뒤에 한 자리만 있는 랜덤한 숫자를 10개 출력하는 프로그램을 작성하세요.
 
 #include <iostream>
 #include <cstdlib>
@@ -9,18 +7,11 @@ using namespace std;
 int main()
 {
     srand(time(0));
-    int frontCnt{0}, backCnt{0};
 
-    for(int i = 0; i < 1000000; i++){
-        int coin = rand() % (1 - 0 + 1);
-        if(coin == 0){
-            frontCnt++;
-        }
-        else{
-            backCnt++;
-        }
+    for(int i = 0; i < 10; i++){
+        double tmp = (rand() % (1000 - 1 + 1) + 1) / 10.0;
+        cout << tmp << " ";
     }
-    cout << "앞면: " << frontCnt << endl;
-    cout << "뒷면: " << backCnt << endl;
+    cout << endl;
     return 0;
 }
