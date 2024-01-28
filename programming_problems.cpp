e5cee1a925a76a10666536e355991bd83fac7733
sjs7049 <sjs7049@gmail.com>
2024-01-28 12:32:01 +0900

    c++ practice 7-1

--- programming_problems.cpp
+++ programming_problems.cpp
@@ -1,20 +1,30 @@
-// 6-11. 도씨 온도를 매개변수로 받아서, 화씨 온도를 리턴하는 함수를 작성하세요.
-// 0, 37, 40, 100도씨가 몇 화씨에 해당하는지 구해서 출력하세요.
-// 화씨 = 도씨 * 180.0 / 100.0 + 32
+// 7-1. int 자료형의 데이터 멤버 x, y와 이러한 데이터 멤버의 접근자 멤버 함수 getX, getY를
+// 갖는 One이라는 클래스를 만드세요. 
 
 #include <iostream>
 using namespace std;
 
-int returnCelsius(double paraFahrenheit){
-    int celsius = (paraFahrenheit - 32) * (100.0 / 180.0);
-    return celsius;
-}
+class One
+{
+public: 
+    One(int ox, int oy)
+    : x(ox), y(oy) {}
+    One()
+    : x(0), y(0) {}
+    int getX(){ return x; }
+    int gety(){ return y; }
+private:
+    int x, y;
+};
 
 int main()
 {
-    cout << returnCelsius(32) << endl;
-    cout << returnCelsius(98.6) << endl;
-    cout << returnCelsius(104) << endl;
-    cout << returnCelsius(212) << endl;
+    One one1(3, 4);
+    One one2;
+    One one3(one1);
+
+    cout << "x = " << one1.getX() << ", y = " << one1.gety() << endl;
+    cout << "x = " << one2.getX() << ", y = " << one2.gety() << endl;
+    cout << "x = " << one3.getX() << ", y = " << one3.gety() << endl;
     return 0;
 }
