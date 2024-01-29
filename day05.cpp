a292b1a8aae91775f07ad4a140805fb520b8da1f
sjs7049 <sjs7049@gmail.com>
2024-01-29 12:55:54 +0900

    c++ program 8-13

--- day05.cpp
+++ day05.cpp
@@ -1,43 +1,55 @@
-
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
-class Student
-{
-public:
-    Student() {}
-    Student(string nameValue, int scoreValue)
-    : name(nameValue), score(scoreValue) {
-        if(scoreValue > 90) { grade = 'A'; }
-        else if(scoreValue > 80) { grade = 'B'; }
-        else if(scoreValue > 70) { grade = 'C'; }
-        else if(scoreValue > 60) { grade = 'D'; }
-        else { grade = 'F'; }
+void findStudentAverage(int const array[][3], int rowsize, int colsize, double averArray[]){
+    for(int i = 0; i < rowsize; i++){
+        int sum = 0;
+        for(int j = 0; j < colsize; j++){
+            sum += array[i][j];
+        }
+        double average = static_cast<double> (sum) / colsize;
+        averArray[i] = average;
     }
-    ~Student() {}
-    void print(){
-        cout << setw(12) << left << name;
-        cout << setw(8) << score;
-        cout << setw(8) << grade << endl;
+}
+
+void findTestAverage(int const array[][3], int rowsize, int colsize, double averArray[]){
+    for(int j = 0; j < colsize; j++){
+        int sum = 0;
+        for(int i = 0; i < rowsize; i++){
+            sum += array[i][j];
+        }
+        double average = static_cast<double> (sum) / rowsize;
+        averArray[j] = average;
     }
-private:
-    string name;
-    int score;
-    char grade;
-};
+}
 
 int main()
 {
-    Student student[5];
-    student[0] = Student("George", 82);
-    student[1] = Student("John", 73);
-    student[2] = Student("Luci", 91);
-    student[3] = Student("Mary", 72);
-    student[4] = Student("Sue", 65);
+    const int rowSize = 5;
+    const int colSize = 3;
 
-    for(int i = 0; i < 5; i++){
-        student[i].print();
+    int scores[rowSize][colSize] = {{82, 65, 72}, {73, 70, 80}, {91, 76, 40}, {72, 72, 68}, {65, 90, 80}};
+    double stuAver[rowSize];
+    double testAver[colSize];
+
+    findStudentAverage(scores, rowSize, colSize, stuAver);
+    findTestAverage(scores, rowSize, colSize, testAver);
+
+    cout << "                  시험 점수        평균" << endl; 
+    cout << "           ---------------------- -------" << endl;
+    for(int i = 0; i < rowSize; i++){
+        cout << "           ";
+        for(int j = 0; j < colSize; j++){
+            cout << scores[i][j] << " ";
+        }
+        cout << fixed << setprecision(2) << "               " << stuAver[i] << endl;
     }
+
+    cout << "최종 점수  ";
+    for(int j = 0; j < colSize; j++){
+        cout << fixed << setprecision(2) << testAver[j] << " ";
+    }
+    cout << endl;
     return 0;
 }
