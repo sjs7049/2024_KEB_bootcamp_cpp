2b5c01b577fd4b44fc9653b34939a46aec0ab29e
sjs7049 <sjs7049@gmail.com>
2024-01-29 15:47:53 +0900

    c++ program 9-11

--- day05.cpp
+++ day05.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int getSum(const int* pArr, int size){
-    int sum = 0;
-    for(int i = 0; i < size; i++){
-        sum += *(pArr++);
+void reverse(int* pArr, int size){
+    int i = 0;
+    int j = size - 1;
+
+    while(i < size / 2){
+        int tmp = *(pArr + i);
+        *(pArr + i) = *(pArr + j);
+        *(pArr + j) = tmp;
+        i++;
+        j--;
     }
-    return sum;
 }
 
 int main()
 {
     int arr[5] = {10, 11, 12, 13, 14};
+    reverse(arr, 5);
 
-    cout << "요소의 합: " << getSum(arr, 5) << endl;
+    cout << "배열 거꾸로 출력: ";
+    for(int i = 0; i < 5; i++){
+        cout << *(arr + i) << " ";
+    }
+    cout << endl;
     return 0;
 }
