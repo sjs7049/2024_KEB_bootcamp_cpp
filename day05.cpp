4920b028710482a12029e4853e3130ae7a7946eb
sjs7049 <sjs7049@gmail.com>
2024-01-29 16:00:23 +0900

    c++ program 9-12

--- day05.cpp
+++ day05.cpp
@@ -1,28 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void reverse(int* pArr, int size){
-    int i = 0;
-    int j = size - 1;
-
-    while(i < size / 2){
-        int tmp = *(pArr + i);
-        *(pArr + i) = *(pArr + j);
-        *(pArr + j) = tmp;
-        i++;
-        j--;
+void print(int(*m)[4], int rows){
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < 4; j++){
+            cout << m[i][j] << " ";
+        }
+        cout << endl;
     }
 }
 
 int main()
 {
-    int arr[5] = {10, 11, 12, 13, 14};
-    reverse(arr, 5);
-
-    cout << "배열 거꾸로 출력: ";
-    for(int i = 0; i < 5; i++){
-        cout << *(arr + i) << " ";
-    }
-    cout << endl;
+    int matrix[3][4] = {{10, 11, 12, 13}, {20, 21, 22, 23}, {31, 32, 33, 34}};
+    print(matrix, 3);
     return 0;
 }
