e782a3473a95d1de7fbfc3e51286c026ee614523
sjs7049 <sjs7049@gmail.com>
2024-01-29 10:56:28 +0900

    c++ program 8-7

--- day05.cpp
+++ day05.cpp
@@ -1,44 +1,25 @@
-
 #include <iostream>
-#include <fstream>
 using namespace std;
+
+void mulByTwo(int array[], int size){
+    for(int i = 0; i < size; i++){
+        array[i] *= 2;
+    }
+}
+
+void print(int array[], int size){
+    for(int i = 0; i < size; i++){
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
 
 int main()
 {
-    ifstream ifo("numbersArray.txt");
-    if(!ifo){
-        cout << "The file could not be opened!" << endl;
-        exit(1);
-    }
+    int numbers[5] = {150, 170, 190, 110, 130};
+    print(numbers, 5);
 
-    const int CAPACITY = 50;
-    int numbers[CAPACITY];
-    int size = 0;
-
-    while(ifo >> numbers[size]){
-        size++;
-    }
-
-    ifo.close();
-
-    int sum = 0;
-    int max{0}, min{101};
-    double average;
-    for(int i = 0; i < size; i++){
-        sum += numbers[i];
-        if(numbers[i] > max){
-            max = numbers[i];
-        }
-        if(numbers[i] < min){
-            min = numbers[i];
-        }
-    }
-
-    average = static_cast<double> (sum) / size;
-    cout << "목록에 숫자가 10개 있습니다." << endl;
-    cout << "합계: " << sum << endl;
-    cout << "평균: " << average << endl;
-    cout << "최솟값: " << min << endl;
-    cout << "최댓값: " << max << endl;
+    mulByTwo(numbers, 5);
+    print(numbers, 5);
     return 0;
 }
