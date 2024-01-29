bc604032a92f3a69614369cd747f00acb60e5c9c
sjs7049 <sjs7049@gmail.com>
2024-01-29 14:01:35 +0900

    c++ program 9-2

--- day05.cpp
+++ day05.cpp
@@ -1,55 +1,22 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
-void findStudentAverage(int const array[][3], int rowsize, int colsize, double averArray[]){
-    for(int i = 0; i < rowsize; i++){
-        int sum = 0;
-        for(int j = 0; j < colsize; j++){
-            sum += array[i][j];
-        }
-        double average = static_cast<double> (sum) / colsize;
-        averArray[i] = average;
-    }
-}
-
-void findTestAverage(int const array[][3], int rowsize, int colsize, double averArray[]){
-    for(int j = 0; j < colsize; j++){
-        int sum = 0;
-        for(int i = 0; i < rowsize; i++){
-            sum += array[i][j];
-        }
-        double average = static_cast<double> (sum) / rowsize;
-        averArray[j] = average;
-    }
+void swap(int& rX, int& rY){
+    int tmp = rX;
+    rX = rY;
+    rY = tmp;
 }
 
 int main()
 {
-    const int rowSize = 5;
-    const int colSize = 3;
+    int x = 10;
+    int y = 20;
 
-    int scores[rowSize][colSize] = {{82, 65, 72}, {73, 70, 80}, {91, 76, 40}, {72, 72, 68}, {65, 90, 80}};
-    double stuAver[rowSize];
-    double testAver[colSize];
+    cout << "스왑하기 전의 x와 y의 값" << endl;
+    cout << "x: " << x << "  " << "y: " << y << endl;
 
-    findStudentAverage(scores, rowSize, colSize, stuAver);
-    findTestAverage(scores, rowSize, colSize, testAver);
-
-    cout << "                  시험 점수        평균" << endl; 
-    cout << "           ---------------------- -------" << endl;
-    for(int i = 0; i < rowSize; i++){
-        cout << "           ";
-        for(int j = 0; j < colSize; j++){
-            cout << scores[i][j] << " ";
-        }
-        cout << fixed << setprecision(2) << "               " << stuAver[i] << endl;
-    }
-
-    cout << "최종 점수  ";
-    for(int i = 0; i < colSize; i++){
-        cout << fixed << setprecision(2) << testAver[i] << " ";
-    }
-    cout << endl;
+    swap(x, y);
+    cout << "스왑 후의 x와 y의 값" << endl;
+    cout << "x: " << x << "  " << "y: " << y << endl;   
     return 0;
 }
