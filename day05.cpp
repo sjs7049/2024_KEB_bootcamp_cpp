36911ec1028fe46d7bed288885fc80aadbe8fe88
sjs7049 <sjs7049@gmail.com>
2024-01-29 10:41:39 +0900

    c++ program 8-5

--- day05.cpp
+++ day05.cpp
@@ -1,43 +1,44 @@
 
 #include <iostream>
+#include <fstream>
 using namespace std;
-
-class Circle
-{
-private:
-    double radius;
-public:
-    Circle(double r)
-    : radius(r) {}
-    Circle()
-    : radius(0.0) {}
-    ~Circle() {}
-    Circle(const Circle& circle)
-    : radius(circle.radius) {}
-
-    void setRadius(double r){
-        radius = r;
-    }
-    double getRadius() const{
-        return radius;
-    }
-    double getPerimeter() const{
-        return 2 * 3.14 * radius;
-    }
-    double getArea() const{
-        return 3.14 * radius * radius;
-    }
-};
 
 int main()
 {
-    const int size = 3;
-    Circle circles[size] = {Circle(3.0), Circle(4.0), Circle(5.0)};
-
-    for(int i = 0; i < size; i++){
-        cout << "circle[" << i << "]의 정보" << endl;
-        cout << "Radius: " << circles[i].getRadius() << ", Area: " << circles[i].getArea()
-            << ", Perimeter: " << circles[i].getPerimeter() << endl;
+    ifstream ifo("numbersArray.txt");
+    if(!ifo){
+        cout << "The file could not be opened!" << endl;
+        exit(1);
     }
+
+    const int CAPACITY = 50;
+    int numbers[CAPACITY];
+    int size = 0;
+
+    while(ifo >> numbers[size]){
+        size++;
+    }
+
+    ifo.close();
+
+    int sum = 0;
+    int max{0}, min{101};
+    double average;
+    for(int i = 0; i < size; i++){
+        sum += numbers[i];
+        if(numbers[i] > max){
+            max = numbers[i];
+        }
+        if(numbers[i] < min){
+            min = numbers[i];
+        }
+    }
+
+    average = static_cast<double> (sum) / size;
+    cout << "목록에 숫자가 10개 있습니다." << endl;
+    cout << "합계: " << sum << endl;
+    cout << "평균: " << average << endl;
+    cout << "최솟값: " << min << endl;
+    cout << "최댓값: " << max << endl;
     return 0;
 }
