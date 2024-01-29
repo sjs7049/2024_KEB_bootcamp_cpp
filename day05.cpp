e6ba328f55f8a2e16470537f4d7e58d40ec4f143
sjs7049 <sjs7049@gmail.com>
2024-01-29 14:47:42 +0900

    c++ program 9-3

--- day05.cpp
+++ day05.cpp
@@ -1,22 +1,74 @@
 #include <iostream>
+#include <cmath>
+#include <cassert>
 using namespace std;
 
-void swap(int& rX, int& rY){
-    int tmp = rX;
-    rX = rY;
-    rY = tmp;
+class Fraction
+{
+public:
+    Fraction(int num, int den = 1)
+    : numerator(num), denominator(den) {
+        normalize();
+    }
+    int getNumerator() const{ return numerator; }
+    int getDenominator() const{ return denominator; }
+    void setNumerator(int num) {
+        numerator = num; 
+        normalize();
+    }
+    void setDenominator(int den) {
+        denominator = den;
+        normalize();
+    }
+    void print() const{
+        cout << numerator << "/" << denominator << endl;
+    }
+
+private:
+    int numerator, denominator;
+    int gcd(int n, int m){
+        int gcd = 1;
+        for(int i = 1; i <= n && i <= m; i++){
+            if(n % i == 0 && m % i == 0){
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+    void normalize(){
+        if(denominator == 0){
+            cout << "잘못된 분모의 값입니다." << endl;
+            assert(false);
+        }
+
+        if(denominator < 0){
+            numerator = -numerator;
+            denominator = -denominator;
+        }
+
+        int divisor = gcd(abs(numerator), abs(denominator));
+        numerator /= divisor;
+        denominator /= divisor;
+    }
+};
+
+Fraction& larger(Fraction& fract1, Fraction& fract2){
+    if((fract1.getNumerator()*fract2.getDenominator()) > (fract1.getDenominator()*fract2.getNumerator())){
+        return fract1;
+    }
+    return fract2;
 }
 
 int main()
 {
-    int x = 10;
-    int y = 20;
+    Fraction fract1(3, 13);
+    Fraction fract2(5, 17);
+    cout << "첫 번째 쌍에서 더 큰 분수: ";
+    larger(fract1, fract2).print();
 
-    cout << "스왑하기 전의 x와 y의 값" << endl;
-    cout << "x: " << x << "  " << "y: " << y << endl;
-
-    swap(x, y);
-    cout << "스왑 후의 x와 y의 값" << endl;
-    cout << "x: " << x << "  " << "y: " << y << endl;   
+    Fraction fract3(4, 9);
+    Fraction fract4(1, 6);
+    cout << "두 번째 쌍에서 더 큰 분수: ";
+    larger(fract3, fract4).print();
     return 0;
 }
