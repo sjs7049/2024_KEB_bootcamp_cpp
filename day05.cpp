9f0aaf62c81876a4334dcd094de87c0a25901799
sjs7049 <sjs7049@gmail.com>
2024-01-29 10:27:21 +0900

    c++ program 8-4

--- day05.cpp
+++ day05.cpp
@@ -1,28 +1,43 @@
-// 8-1. 숫자 배열을 거꾸로 출력하기
 
 #include <iostream>
 using namespace std;
 
+class Circle
+{
+private:
+    double radius;
+public:
+    Circle(double r)
+    : radius(r) {}
+    Circle()
+    : radius(0.0) {}
+    ~Circle() {}
+    Circle(const Circle& circle)
+    : radius(circle.radius) {}
+
+    void setRadius(double r){
+        radius = r;
+    }
+    double getRadius() const{
+        return radius;
+    }
+    double getPerimeter() const{
+        return 2 * 3.14 * radius;
+    }
+    double getArea() const{
+        return 3.14 * radius * radius;
+    }
+};
+
 int main()
 {
-    const int CAPACITY = 10;
-    int numbers[CAPACITY];
-    int size;
-    do
-    {
-        cout << "크기를 입력해주세요(1~10): ";
-        cin >> size;
-    } while (size < 1 || size > 10);
-    
-    cout << size << "개의 숫자를 입력해주세요: ";
-    for(int i = 0; i < size; i++){
-        cin >> numbers[i];
-    }
+    const int size = 3;
+    Circle circles[size] = {Circle(3.0), Circle(4.0), Circle(5.0)};
 
-    cout << "입력한 숫자들을 거꾸로 출력: ";
-    for(int i = size - 1; i >= 0; i--){
-        cout << numbers[i] << " ";
+    for(int i = 0; i < size; i++){
+        cout << "circle[" << i << "]의 정보" << endl;
+        cout << "Radius: " << circles[i].getRadius() << ", Area: " << circles[i].getArea()
+            << ", Perimeter: " << circles[i].getPerimeter() << endl;
     }
-    cout << endl;
     return 0;
 }
