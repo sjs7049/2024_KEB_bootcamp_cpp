66c2042125539e64e456895d20b62a4852430f8c
sjs7049 <sjs7049@gmail.com>
2024-01-30 11:31:14 +0900

    c++ program 10-22

--- day06.cpp
+++ day06.cpp
@@ -2,10 +2,32 @@
 #include <string>
 using namespace std;
 
+void reverse(string& strg){
+    string temp(strg);
+    int size = strg.size();
+
+    for(int i = 0; i < size; i++){
+        strg[i] = temp[size - 1 - i];
+    }
+}
+
+bool isPalindrome(string& strg){
+    string temp(strg);
+    reverse(temp);
+    return(temp == strg);
+}
+
 int main()
 {
-    string strg("The C++ language is fun to work with.");
-    cout << strg.substr(8) << endl;
-    cout << strg.substr(4,12) << endl;
-    return 0;
+   string strg;
+   cout << "문자열을 입력하세요: ";
+   getline(cin, strg);
+
+   if(isPalindrome(strg)){
+    cout << strg << "은 회문입니다." << endl;
+   }
+   else{
+    cout << strg << "은 회문이 아닙니다." << endl;
+   }
+   return 0;
 }
