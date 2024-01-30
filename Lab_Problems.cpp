3b25ca7305e5bfcaf7f0ffe5f366ae22ed558309
sjs7049 <sjs7049@gmail.com>
2024-01-30 12:01:13 +0900

    c++ program 10-26

--- Lab_Problems.cpp
+++ Lab_Problems.cpp
@@ -1,112 +1,54 @@
-// Matrix 클래스 만들기
-/*                                              
-행렬의 크기와 힙에 생성할 행렬을 가리킬 포인터만 private 멤버로 선언, 이외의 모든 멤버 함수는 public 멤버로 선언                                     
-생성자는 힙에 배열을 생성하고, 소멸자는 힙에서 배열을 제거                     
-setup 멤버 함수는 행렬을 랜덤한 값으로 초기화                                  
-이외에도 addition, subtraction, multiplication, print 멤버 함수를 선언
-*/
+// 문자열과 관련된 사용자 정의 함수 만들기  
 
 #include <iostream>
-#include <cstdlib>
-#include <iomanip>
+#include <string>
 using namespace std;
 
-class Matrix  
-{  
-public:                                                                                                                                         
-    Matrix(int rowsize, int colsize)
-    : rowSize(rowsize), colSize(colsize){
-        ptr = new int* [rowSize];
-        for(int i = 0; i < rowSize; i++){
-            ptr[i] = new int[i];
-        }
-    }              
-    ~Matrix(){
-        for(int i = 0; i < rowSize; i++){
-            delete ptr[i];
-        }
-        delete[] ptr;
-    }
-    void setup(){
-        // 1~5 사이의 수 랜덤으로
+void pushFront(string& strg, char c){
+    string temp(1, c);
+    strg.insert(0, temp);
+}
 
-        for(int i = 0; i < rowSize; i++){
-            for(int j = 0; j < colSize; j++){
-                ptr[i][j] = rand() % 5 + 1; 
-            }
-        }
-    }
-    void add(const Matrix& second, Matrix& result) const{
-        for(int i = 0; i < rowSize; i++){
-            for(int j = 0; j < second.colSize; j++){
-                result.ptr[i][j] = ptr[i][j] + second.ptr[i][j];
-            }
-        }
-    }
-    void subtract(const Matrix& second, Matrix& result) const{
-        for(int i = 0; i < rowSize; i++){
-            for(int j = 0; j < second.colSize; j++){
-                result.ptr[i][j] = ptr[i][j] - second.ptr[i][j];
-            }
-        }
-    }
-    void multiply(const Matrix& second, Matrix& result) const{
-        for(int i = 0; i < rowSize; i++){
-            for(int j = 0; j < second.colSize; j++){
-                result.ptr[i][j] = 0;
-                for(int k = 0; k < colSize; k++){
-                    result.ptr[i][j] += ptr[i][k] * second.ptr[k][j];
-                }
-            }
-        }
-    }
-    void print() const{
-        for(int i = 0; i < rowSize; i++){
-            for(int j = 0; j <colSize; j++){
-                cout << setw(5) << ptr[i][j];
-            }
-            cout << endl;
-        }
-        cout << endl;
-    }
-private:  
-    int rowSize;                    
-    int colSize;  
-    int** ptr;
-};  
+void pushBack(string& strg, char c){
+    string temp(1, c);
+    strg.append(temp);
+}
+
+char popFront(string& strg){
+    int idx = 0;
+    char temp = strg[idx];
+    strg.erase(idx, 1);
+    return temp;
+}
+
+char popBack(string& strg){
+    int idx = strg.size() - 1;
+    char temp = strg[idx];
+    strg.erase(idx, 1);
+    return temp;
+}
 
 int main()
 {
-    cout << "matrix1" << endl;
-    Matrix matrix1(3,4);
-    matrix1.setup();
-    matrix1.print();
-
-    cout << "matrix2" << endl;
-    Matrix matrix2(3,4);
-    matrix2.setup();
-    matrix2.print();
-
-    cout << "matrix3" << endl;
-    Matrix matrix3(4,2);
-    matrix3.setup();
-    matrix3.print();
-
-
-    cout << "matrix1 + matrix2의 결과" << endl;
-    Matrix addMatrix(3,4);
-    matrix1.add(matrix2, addMatrix);
-    addMatrix.print();
-
-    cout << "matrix1 - matrix2의 결과" << endl;
-    Matrix subMatrix(3,4);
-    matrix1.subtract(matrix2, subMatrix);
-    subMatrix.print();
-
-    cout << "matrix1 * matrix3의 결과" << endl;
-    Matrix mulMatrix(3,2);
-    matrix1.multiply(matrix3, mulMatrix);
-    mulMatrix.print();
+    string strg("abcdefgh");
     
+    cout << "pushFront 전의 문자열: " << strg << endl;
+    pushFront(strg, 'A');
+    cout << "pushFront 후의 문자열: " << strg << endl;
+
+    cout << "pushBack 전의 문자열: " << strg << endl;
+    pushBack(strg, 'Z');
+    cout << "pushBack 후의 문자열: " << strg << endl;
+
+    cout << "popFront 전의 문자열: " << strg << endl;
+    char c1 = popFront(strg);
+    cout << "popFront 후의 문자열: " << strg << endl;
+    cout << "추출한 문자: " << c1 << endl;
+
+    cout << "popBack 전의 문자열: " << strg << endl;
+    char c2 = popBack(strg);
+    cout << "popBack 후의 문자열: " << strg << endl;
+    cout << "추출한 문자: " << c2 << endl;
+
     return 0;
 }
