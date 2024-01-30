f5e1ad633a7e4a523bd48350f124d660daae5b29
sjs7049 <sjs7049@gmail.com>
2024-01-30 11:15:55 +0900

    c++ program 10-18

--- day06.cpp
+++ day06.cpp
@@ -2,23 +2,10 @@
 #include <string>
 using namespace std;
 
-void reverse(string& strg){
-    string tmp(strg);
-    int size = strg.size();
-    
-    for(int i = 0; i < size; i++){
-        strg[i] = tmp[size - 1 - i];
-    }
-}
-
 int main()
 {
-    string strg;
-    cout << "문자열을 입력하세요: ";
-    getline(cin, strg);
-    cout << "원본 문자열: " << strg << endl;
-
-    reverse(strg);
-    cout << "뒤집은 문자열: " << strg << endl;
-    return 0;   
+    string strg("The C++ language is fun to work with.");
+    cout << strg.substr(8) << endl;
+    cout << strg.substr(4,12) << endl;
+    return 0;
 }
