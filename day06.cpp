5f33844da72f5aa5c53e3ad6e802c0b83df16f05
sjs7049 <sjs7049@gmail.com>
2024-01-30 10:40:48 +0900

    c++ program 10-10

--- day06.cpp
+++ day06.cpp
@@ -4,16 +4,9 @@ using namespace std;
 
 int main()
 {
-    const char* str1 = "Hello Alice.";
-    const char* str2 = "Hello John.";
-    const char* str3 = "Hello Betsy.";
-    
-    cout << "str1과 str2 비교하기: ";
-    cout  << strcmp(str1, str2) << endl;
-    cout << "str2와 str3 비교하기: ";
-    cout << strcmp(str2, str3) << endl;
-
-    cout << "str1과 str2의 앞 5문자만 비교하기: ";
-    cout << strncmp(str1, str2, 5) << endl;
+    char str1[20] = "This is ";
+    const char* str2 = "a string.";
+    strcat(str1, str2);
+    cout << "str1: " << str1 << endl;
     return 0;
 }
