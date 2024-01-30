8212c1609109c18d6c2a4437823e47872258db00
sjs7049 <sjs7049@gmail.com>
2024-01-30 10:26:45 +0900

    c++ program 10-7

--- day06.cpp
+++ day06.cpp
@@ -4,14 +4,10 @@ using namespace std;
 
 int main()
 {
-    char str[] = "Hello friends.";
+    char str[] = "Hello friends of mine.";
 
-    char* cptr = strchr(str, 'e');
-    *cptr = 'E';
-    cout << "첫 번째 변경 후의 str: " << str << endl;
-
-    cptr = strrchr(str, 'e');
-    *cptr = 'E';
-    cout << "두 번째 변경 후의 str: " << str << endl;
+    char* sPtr = strstr(str, "friends");
+    cout << sPtr << ", " << str << endl;
+    cout << "서브 문자열의 시작 인덱스: " << str - sPtr << endl;
     return 0;
 }
